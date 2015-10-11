ccf07b87bfd6cfd26a437861f67291b2c9a52ce7
Robert Irwin <robert.irwin@temple.edu>
2015-10-11 10:51:55 -0400

    Fixed an issue with interpreting the data.  The data is now opened with universal line mode.  Also, tabs were replaced by 4 spaces.

--- objectcreation.py
+++ objectcreation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 class Customer(object):
-	"""__init() functions as the class constructor"""
-	def __init__(customer,id=None, state=None, zipcode=None, segment=None):
-		customer.id = id
-		customer.state = state
-		customer.zipcode = zipcode
-		customer.segment = segment
+    """__init() functions as the class constructor"""
+    def __init__(customer,id=None, state=None, zipcode=None, segment=None):
+        customer.id = id
+        customer.state = state
+        customer.zipcode = zipcode
+        customer.segment = segment
 
 #	def __del__(self):
 #		customer.id = None
@@ -16,15 +16,15 @@ class Customer(object):
 
 
 class Order(object):
-	def __init__(order, number = None, line = None, customerid = None, productnum = None, platform = None, date = None, time = None, amount = None):
-		order.number = number
-		order.line = line
-		order.customerid = customerid
-		order.productnum = productnum
-		order.platform = platform
-		order.date = date
-		order.time = time
-		order.amount = amount
+    def __init__(order, number = None, line = None, customerid = None, productnum = None, platform = None, date = None, time = None, amount = None):
+        order.number = number
+        order.line = line
+        order.customerid = customerid
+        order.productnum = productnum
+        order.platform = platform
+        order.date = date
+        order.time = time
+        order.amount = amount
 
 #	def __del__(self):
 #		order.number = None
@@ -38,14 +38,14 @@ class Order(object):
 
 
 class Media(object):
-	def __init__(Media, productid = None, date = None, starttime = None, endtime = None, airtime = None, Host1 = None, Host2 = None):
-		Media.productid = productid
-		Media.date = date
-		Media.starttime = starttime
-		Media.endtime = endtime
-		Media.airtime = airtime
-		Media.host1 = Host1
-		Media.host2 = Host2
+    def __init__(Media, productid = None, date = None, starttime = None, endtime = None, airtime = None, Host1 = None, Host2 = None):
+        Media.productid = productid
+        Media.date = date
+        Media.starttime = starttime
+        Media.endtime = endtime
+        Media.airtime = airtime
+        Media.host1 = Host1
+        Media.host2 = Host2
 
 
 
@@ -53,11 +53,11 @@ class Media(object):
 print
 
 import csv
-f = open('customer_master.csv')
+f = open('customer_master.csv', 'rU')
 csv_f = csv.reader(f)
 customerlist = []
 for row in csv_f:
-	customerlist.append(Customer(row[0],row[1],row[2],row[3]))
+    customerlist.append(Customer(row[0],row[1],row[2],row[3]))
 
 
 # Test that data is stored correctly
@@ -79,7 +79,7 @@ f = open('order_master.csv')
 csv_f = csv.reader(f)
 orderlist = []
 for row in csv_f:
-	orderlist.append(Order(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
+    orderlist.append(Order(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
 
 #print( "Order Number:" + orderlist[n].number + "\nOrder Line:" + orderlist[n].line + "\nCustomer ID:" + orderlist[n].customerid )
 #print( "Product Number:" + orderlist[n].productnum + "\nPlatform:" + orderlist[n].platform + "\nDate:" + orderlist[n].date) 
@@ -91,7 +91,7 @@ f = open('product_airtime.csv')
 csv_f = csv.reader(f)
 medialist = []
 for row in csv_f:
-	medialist.append(Media(row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
+    medialist.append(Media(row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
 
 
 #print( "Product Number:" + medialist[n].productid + "\nAir Date:" + medialist[n].date + "\nStart Time:" + medialist[n].starttime)
